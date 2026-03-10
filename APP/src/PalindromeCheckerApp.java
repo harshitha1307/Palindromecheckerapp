90f0d40efc379f3d90c0a1f27c549be9f154398f
harshitha1307 <pharshitha051@gmail.com>
2026-03-10 10:16:24 +0530

    harshitha added UC1 - Application Entry & Welcome Message

---
+++ APP/src/PalindromeCheckerApp.java
@@ -0,0 +1,19 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // welcome message
+        System.out.println("===================================");
+        System.out.println(" Welcome to Palindrome Checker App ");
+        System.out.println("===================================");
+
+        //  application details
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Version          : 1.0");
+
+
+        System.out.println("The program is ready to check palindromes.");
+        System.out.println("===================================");
+    }
+}
+
